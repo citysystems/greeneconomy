319ccfe0ff6af87274493d779c90f4acacade6d3
maxo16 <maxo@stanford.edu>
2019-08-30 15:59:28 -0700

    general update

--- VMT_calc/OSRM_isochrone_route_comparison.R
+++ VMT_calc/OSRM_isochrone_route_comparison.R
@@ -13,6 +13,7 @@ library(ggmap)
 library(censusr)
 library(maptools)
 library(rgeos)
+library(microbenchmark)
 
 ##########
 
@@ -61,78 +62,141 @@ safegraphplaces <- safegraphplaces[!is.na(safegraphplaces$sub_category), ]
 
 # OSRM Approach #1: OSRM Isochrones
 
-# Creation of the "break" times used for the iscochrone creation.
-break_times <- c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 40, 50, 60)
+function_OSRMisochrone <- function(blocks_amount_for_testing){
 
-# Creation of a data frame that holds in all block group isochrone - safegraphplaces points intersections.
-isochrone_block_Stockton <- data.frame(stringsAsFactors = FALSE)
-
-# This first for loop chooses each block group for analysis.
-for(counter_block in sspz_centroid$GEOID){
-
-  # The following code creates an isochrone at the centroid of the block group being analyzed.
-  isochrone_block <- osrmIsochrone(sspz_centroid[(sspz_centroid$GEOID == counter_block[1]),], breaks = break_times, returnclass = "sf")
-  isochrone_block <- st_set_crs(isochrone_block, '+proj=longlat +datum=WGS84')
-
-  # This second for loop chooses each amenity for analysis.  
-  for(counter_amenity in unique(safegraphplaces$sub_category)){
+  # These variables (1) intake the iterations factor and (2) start the counting of each iteration. 
+  n <- blocks_amount_for_testing
+  count <- 0
   
-    # The following code chooses the safegraphplaces points associated with the sub-category being analyzed.
-    isochrone_block_points <- st_as_sf(safegraphplaces[safegraphplaces$sub_category == counter_amenity[1], ], coords = c("longitude", "latitude"), crs = 4326)
+  # Creation of the "break" times used for the iscochrone creation.
+  break_times <- c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 40, 50, 60)
+  # break_times <- c(0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 
+  #                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30)
+  
+  # Creation of a data frame that holds in all block group isochrone - safegraphplaces points intersections.
+  isochrone_block_Stockton <- data.frame(stringsAsFactors = FALSE)
+
+  # This first for loop chooses each block group for analysis.
+  for(counter_block in sspz_centroid$GEOID){
+
+    # The following code creates an isochrone at the centroid of the block group being analyzed.
+    # isochrone_block <- osrmIsochrone(sspz_centroid[(sspz_centroid$GEOID == counter_block),], breaks = break_times, returnclass = "sf")
+    isochrone_block <- osrmIsochrone(sspz_centroid[(sspz_centroid$GEOID == counter_block),], breaks = break_times, res = 2000, returnclass = "sf")
+    isochrone_block <- st_set_crs(isochrone_block, '+proj=longlat +datum=WGS84')
+    mapview(isochrone_block)
     
-    # The following code performs an isochrone intersection between the block group isochrone and the 
-    isochrone_block_intersection <- cbind(counter_block[1], data.frame(st_intersection(isochrone_block_points, isochrone_block)), stringsAsFactors = FALSE)
-   
+    # The following code chooses the safegraphplaces points associated with the sub-category being analyzed.
+    isochrone_block_points <- st_as_sf(safegraphplaces, coords = c("longitude", "latitude"), crs = 4326)
+    
+    # The following code performs the intersection between the safegraph points and the generated isochrone block with time bands.
+    isochrone_block_intersection <- data.frame(st_intersection(isochrone_block_points, isochrone_block), stringsAsFactors = FALSE)
+    
+    ##########
+    # missing_safegraphplaces <- safegraphplaces[!(safegraphplaces$safegraph_place_id %in% isochrone_block_intersection$safegraph_place_id), ]
+    # isochrone_block_intersection <- rbind(isochrone_block_intersection, missing_safegraphplaces)
+    ##########
+    
+    # The following code associates the block being analyzed (through its FIPS code) to the isochrone block intersection with the safegraphplaces.
+    isochrone_block_intersection <- cbind(counter_block, isochrone_block_intersection)
+    
     # The following code take the individual block group isochrone - safegraphplaces points analysis and inserts the result into an aggregate data frame.
     isochrone_block_Stockton <- rbind(isochrone_block_Stockton, isochrone_block_intersection)
-     
+        
+    # # I have commented this code out because, though it helps redue the potential for error, the specific error will almost never be found.
+    # # By not including this code, though these will be potential for this error, we will enhance computational time. 
+    #
+    # # This code is necessary if there aren't any safegraphplaces within the isochrones. 
+    # if(nrow(isochrone_block_intersection) != 0){
+    #   
+    #   # The following code performs an isochrone intersection between the block group isochrone and the 
+    #   isochrone_block_intersection <- cbind(counter_block, isochrone_block_intersection)
+    #   
+    #   # The following code take the individual block group isochrone - safegraphplaces points analysis and inserts the result into an aggregate data frame.
+    #   isochrone_block_Stockton <- rbind(isochrone_block_Stockton, isochrone_block_intersection)
+    #   
+    # }
+    
+    # This is evaluating each iteration.
+    count <- count + 1
+  
+    # Once I have hit the amount of block iterations that I am interested in testing, this breaks the loop, and thus breaking the analysis. 
+    if(count == n){break}
+  
   }
   
 }
 
-map_test <- mapview(isochrone_block_points) + mapview(isochrone_block)
+blocks_amount_for_testing <- 1
+res_OSRMisochrone <- microbenchmark(function_OSRMisochrone(blocks_amount_for_testing), times = 1L, unit = "s")
+res_OSRMisochrone
+
+maptest <- mapview(isochrone_block) + mapview(isochrone_block_points)
 
 ##########
 
 # OSRM Approach #2: OSRM Routing
 
-# Creation of a data frame that holds in all block group isochrone - safegraphplaces points intersections.
-isochrone_block_Stockton <- data.frame(stringsAsFactors = FALSE)
-test <- data.frame()
+function_OSRMrouting <- function(blocks_amount_for_testing){
 
-count <- 0
+  # These variables (1) intake the iterations factor and (2) start the counting of each iteration. 
+  n <- blocks_amount_for_testing
+  count <- 0
 
-# This first for loop chooses each block group for analysis.
-for(counter_block in sspz_centroid$GEOID){
+  # Creation of a data frame that holds in all block group isochrone - safegraphplaces points intersections.
+  route_OD_Stockton <- data.frame(stringsAsFactors = FALSE)
+
+  # This first for loop chooses each block group for analysis.
+  for(counter_block in sspz_centroid$GEOID){
   
-  # The following code creates an isochrone at the centroid of the block group being analyzed.
-  bg_centroid <- sspz_centroid[(sspz_centroid$GEOID == counter_block),]
+    # The following code creates an isochrone at the centroid of the block group being analyzed.
+    bg_centroid <- sspz_centroid[(sspz_centroid$GEOID == counter_block),]
   
-  # This second for loop chooses each amenity for analysis.  
-  for(counter_amenity in unique(safegraphplaces$sub_category)){
+    # This second for loop chooses each amenity for analysis.  
+    for(counter_amenity in unique(safegraphplaces$sub_category)){
     
-    # The following code chooses the safegraphplaces points associated with the sub-category being analyzed.
-    safegraph_places <- st_as_sf(safegraphplaces[safegraphplaces$sub_category == counter_amenity, ], coords = c("longitude", "latitude"), crs = 4326)
+      # The following code chooses the safegraphplaces points associated with the sub-category being analyzed.
+      safegraph_places <- st_as_sf(safegraphplaces[safegraphplaces$sub_category == counter_amenity, ], coords = c("longitude", "latitude"), crs = 4326)
     
-      for(counter_point in nrow(isochrone_block_points)){
+        for(counter_point in 1:nrow(safegraph_places)){
         
-        safegraph_point <- safegraph_places[counter_point, ]
+          safegraph_point <- safegraph_places[counter_point, ]
+          safegraph_point_filtered <- dplyr::select(safegraph_point, c("safegraph_place_id", "location_name", "sub_category", "naics_code",
+                                                                       "street_address", "city", "state", "zip_code", "phone_number", "geometry"))
         
-        test <- rbind(test, osrmRoute(src = bg_centroid, dst = safegraph_point, overview = FALSE))
+          OD_route <- t(osrmRoute(src = bg_centroid, dst = safegraph_point_filtered, overview = FALSE))
+          OD_safegraph_route <- data.frame( cbind( counter_block, safegraph_point_filtered, OD_route ), stringsAsFactors = FALSE)
+
+          route_OD_Stockton <- rbind(route_OD_Stockton, OD_safegraph_route)
         
-        count <- count + 1
-        print(count)
+        }
         
-      }
-    
-    # The following code performs an isochrone intersection between the block group isochrone and the 
-    # isochrone_block_intersection <- cbind(counter_block[1], data.frame(st_intersection(isochrone_block_points, isochrone_block)), stringsAsFactors = FALSE)
-    
-    # The following code take the individual block group isochrone - safegraphplaces points analysis and inserts the result into an aggregate data frame.
-    # isochrone_block_Stockton <- rbind(isochrone_block_Stockton, isochrone_block_intersection)
-    
+    }
+  
+    # This is the expression to be evaluted with the microbenchmark test.
+    test_microbenchmark <- route_OD_Stockton
+  
+    # This is evaluating each iteration.
+    count <- count + 1
+  
+    # Once I have hit the amount of block iterations that I am interested in testing, this breaks the loop, and thus breaking the analysis. 
+    if(count == n){break}
+  
   }
-  
-}
 
+}
+  
 ##########
+
+# Evaluation of the time it takes to run OSRM isochrone and OSRM routing for the above two functions.
+# I will run these functions for both "function_OSRMisochrone" and "function_OSRMrouting".
+# I have chosen to run these functions for the first 20 blocks in the sspz.
+
+blocks_amount_for_testing <- 1
+
+res_OSRMisochrone <- microbenchmark(function_OSRMisochrone(blocks_amount_for_testing), times = 1L, unit = "s")
+print(res_OSRMisochrone)
+boxplot(res_OSRMisochrone)
+
+res_OSRMrouting <- microbenchmark(function_OSRMrouting(blocks_amount_for_testing), times = 1L, unit = "s")
+print(res_OSRMrouting)
+boxplot(res_OSRMrouting)
