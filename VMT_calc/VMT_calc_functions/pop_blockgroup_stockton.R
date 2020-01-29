9519274a6b9f657cc891fd0948d0ba6baa9335eb
maxo16 <maxo@stanford.edu>
2020-01-28 22:35:41 -0800

    general update

--- VMT_calc/VMT_calc_functions/pop_blockgroup_stockton.R
+++ VMT_calc/VMT_calc_functions/pop_blockgroup_stockton.R
@@ -8,7 +8,35 @@ pop_blockgroup_stockton <- function(month_hps){
   stockton_bgs <- stockton_bgs %>% dplyr::select(GEOID)
   colnames(stockton_bgs) <- c("origin", "geometry")
   
-  pop_blockgroup_stockton <-
+  ### pop_blockgroup_stockton <- getCensus(name = "acs/acs5", vintage = 2017, key = "c8aa67e4086b4b5ce3a8717f59faa9a28f611dab",
+  ###                                      vars = c("B00001_001E"), region = "block group:*", regionin = "state:06+county:077")
+  
+  ### pop_blockgroup_stockton_B23025 <-
+  ###   getCensus(
+  ###     key = "c8aa67e4086b4b5ce3a8717f59faa9a28f611dab",
+  ###     name = "acs/acs5",
+  ###     vintage = 2018,
+  ###     vars = c("B23025_001E", "B23025_002E", "B23025_003E", "B23025_004E", "B23025_005E", "B23025_006E", "B23025_007E"),
+  ###     region = "block group:*",
+  ###     regionin = "state:06+county:077"
+  ###   ) %>%
+  ###   mutate(
+  ###     total = B23025_001E,
+  ###     total_laborforce = B23025_002E,
+  ###     total_laborforce_civilian = B23025_003E,
+  ###     total_laborforce_civilian_empl = B23025_004E,
+  ###     total_laborforce_civilian_not_empl = B23025_005E,
+  ###     total_laborforce_civilian_armed_forces = B23025_006E,
+  ###     total_laborforce_civilian_not_in_labor = B23025_007E
+  ###   ) %>%
+  ###   dplyr::select(
+  ###     total_laborforce_civilian_empl,
+  ###     total_laborforce_civilian_not_empl,
+  ###     total_laborforce_civilian_armed_forces,
+  ###     total_laborforce_civilian_not_in_labor
+  ###   )
+  
+  pop_blockgroup_stockton <- # pop_blockgroup_stockton_B23027
     getCensus(
       key = "c8aa67e4086b4b5ce3a8717f59faa9a28f611dab",
       name = "acs/acs5",
@@ -50,6 +78,7 @@ pop_blockgroup_stockton <- function(month_hps){
     )
 
   pop_blockgroup_stockton <- data.frame(cbind(paste(pop_blockgroup_stockton[, 1], pop_blockgroup_stockton[, 2], pop_blockgroup_stockton[, 3], pop_blockgroup_stockton[, 4], sep = ""), rowSums(pop_blockgroup_stockton[, 5:11])), stringsAsFactors = FALSE)
+  # pop_blockgroup_stockton <- data.frame(cbind(paste(pop_blockgroup_stockton[, 1], pop_blockgroup_stockton[, 2], pop_blockgroup_stockton[, 3], pop_blockgroup_stockton[, 4], sep = ""), pop_blockgroup_stockton[, 5]), stringsAsFactors = FALSE)
   colnames(pop_blockgroup_stockton) <- c("origin", "origin_population")
 
   pop_blockgroup_stockton <- left_join(stockton_bgs, pop_blockgroup_stockton, by = "origin")
